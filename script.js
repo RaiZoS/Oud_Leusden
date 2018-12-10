67f5a5bf4253f5c5c1da2755f4aa1a2eb681a422
Ingmar Simonse <ingmar.simonse@gmail.com>
2018-12-10 10:41:55 +0100

    imageslider.html update 1.5

--- script.js
+++ script.js
@@ -139,3 +139,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
