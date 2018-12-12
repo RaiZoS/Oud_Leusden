ea8dffdf74ab08b8904756ea1a6c01ea081eb7fb
Ingmar Simonse <ingmar.simonse@gmail.com>
2018-12-12 10:19:48 +0100

    css toegevoegd

--- script.js
+++ script.js
@@ -156,3 +156,25 @@ function carousel() {
 
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" opacity-off", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " opacity-off";
+  }
